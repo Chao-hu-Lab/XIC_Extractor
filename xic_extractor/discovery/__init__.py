9fa28fa369fe76961d09de619fdacac8b9ab7e39
bosschen0429 <j25057175@gmail.com>
2026-05-09 02:38:50 +0800

    feat: collect discovery neutral loss seeds

--- xic_extractor/discovery/__init__.py
+++ xic_extractor/discovery/__init__.py
@@ -1,1 +1,5 @@
 """Untargeted discovery models and contracts."""
+
+from xic_extractor.discovery.ms2_seeds import MS2ScanSource, collect_strict_nl_seeds
+
+__all__ = ("MS2ScanSource", "collect_strict_nl_seeds")
