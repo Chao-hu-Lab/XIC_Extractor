403f4125354d1462c448ff21c48fa06f87444057
bosschen0429 <j25057175@gmail.com>
2026-05-09 07:56:32 +0800

    feat: group discovery neutral loss seeds

--- xic_extractor/discovery/__init__.py
+++ xic_extractor/discovery/__init__.py
@@ -1,5 +1,6 @@
 """Untargeted discovery models and contracts."""
 
+from xic_extractor.discovery.grouping import group_discovery_seeds
 from xic_extractor.discovery.ms2_seeds import MS2ScanSource, collect_strict_nl_seeds
 
-__all__ = ("MS2ScanSource", "collect_strict_nl_seeds")
+__all__ = ("MS2ScanSource", "collect_strict_nl_seeds", "group_discovery_seeds")
